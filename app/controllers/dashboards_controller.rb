da726fa78ffacf0f268ed6e5a6c592abd5e431c9
gary80212@gmail.com <gary80212@gmail.com>
2018-09-06 22:41:21 +0800

    delete @graph in dashboards_controller

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -3,8 +3,6 @@ class DashboardsController < ApplicationController
 
   def index 
     @report = Mailchimp.mailchimp_report
-    @graph = User.last.facebook.get_object("278666028863859/insights/page_content_activity/day")
-    binding.pry
   end
 
   def ga
