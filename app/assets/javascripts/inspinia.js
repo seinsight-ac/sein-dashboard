8f0fa740298bc9809f99c0520117e3bd0ae10ad6
gary80212@gmail.com <gary80212@gmail.com>
2018-09-11 01:44:12 +0800

    Indentation inspinia.js, topnavbar.html

--- app/assets/javascripts/inspinia.js
+++ app/assets/javascripts/inspinia.js
@@ -186,105 +186,104 @@ $(window).bind("resize", function () {
 // Local Storage functions
 // Set proper body class and plugins based on user configuration
 $(document).ready(function () {
-    if (localStorageSupport()) {
+	if (localStorageSupport()) {
 
-        var collapse = localStorage.getItem("collapse_menu");
-        var fixedsidebar = localStorage.getItem("fixedsidebar");
-        var fixednavbar = localStorage.getItem("fixednavbar");
-        var boxedlayout = localStorage.getItem("boxedlayout");
-        var fixedfooter = localStorage.getItem("fixedfooter");
+		var collapse = localStorage.getItem("collapse_menu");
+		var fixedsidebar = localStorage.getItem("fixedsidebar");
+		var fixednavbar = localStorage.getItem("fixednavbar");
+		var boxedlayout = localStorage.getItem("boxedlayout");
+		var fixedfooter = localStorage.getItem("fixedfooter");
 
-        var body = $('body');
+		var body = $('body');
 
-        if (fixedsidebar == 'on') {
-            body.addClass('fixed-sidebar');
-            $('.sidebar-collapse').slimScroll({
-                height: '100%',
-                railOpacity: 0.9
-            });
-        }
+		if (fixedsidebar == 'on') {
+			body.addClass('fixed-sidebar');
+			$('.sidebar-collapse').slimScroll({
+				height: '100%',
+				railOpacity: 0.9
+			});
+		}
 
-        if (collapse == 'on') {
-            if (body.hasClass('fixed-sidebar')) {
-                if (!body.hasClass('body-small')) {
-                    body.addClass('mini-navbar');
-                }
-            } else {
-                if (!body.hasClass('body-small')) {
-                    body.addClass('mini-navbar');
-                }
+		if (collapse == 'on') {
+			if (body.hasClass('fixed-sidebar')) {
+				if (!body.hasClass('body-small')) {
+					body.addClass('mini-navbar');
+				}
+			} else {
+				if (!body.hasClass('body-small')) {
+					body.addClass('mini-navbar');
+				}
+			}
+		}
 
-            }
-        }
+		if (fixednavbar == 'on') {
+			$(".navbar-static-top").removeClass('navbar-static-top').addClass('navbar-fixed-top');
+			body.addClass('fixed-nav');
+		}
 
-        if (fixednavbar == 'on') {
-            $(".navbar-static-top").removeClass('navbar-static-top').addClass('navbar-fixed-top');
-            body.addClass('fixed-nav');
-        }
+		if (boxedlayout == 'on') {
+			body.addClass('boxed-layout');
+		}
 
-        if (boxedlayout == 'on') {
-            body.addClass('boxed-layout');
-        }
-
-        if (fixedfooter == 'on') {
-            $(".footer").addClass('fixed');
-        }
-    }
+		if (fixedfooter == 'on') {
+			$(".footer").addClass('fixed');
+		}
+	}
 });
 
 // check if browser support HTML5 local storage
 function localStorageSupport() {
-    return (('localStorage' in window) && window['localStorage'] !== null)
+	return (('localStorage' in window) && window['localStorage'] !== null)
 }
 
 // For demo purpose - animation css script
 function animationHover(element, animation) {
-    element = $(element);
-    element.hover(
-        function () {
-            element.addClass('animated ' + animation);
-        },
-        function () {
-            //wait for animation to finish before removing classes
-            window.setTimeout(function () {
-                element.removeClass('animated ' + animation);
-            }, 2000);
-        });
+	element = $(element);
+	element.hover(
+		function () {
+			element.addClass('animated ' + animation);
+		},
+		function () {
+			//wait for animation to finish before removing classes
+			window.setTimeout(function () {
+				element.removeClass('animated ' + animation);
+			}, 2000);
+		});
 }
 
 function SmoothlyMenu() {
-    if (!$('body').hasClass('mini-navbar') || $('body').hasClass('body-small')) {
-        // Hide menu in order to smoothly turn on when maximize menu
-        $('#side-menu').hide();
-        // For smoothly turn on menu
-        setTimeout(
-            function () {
-                $('#side-menu').fadeIn(400);
-            }, 200);
-    } else if ($('body').hasClass('fixed-sidebar')) {
-        $('#side-menu').hide();
-        setTimeout(
-            function () {
-                $('#side-menu').fadeIn(400);
-            }, 100);
-    } else {
-        // Remove all inline style from jquery fadeIn function to reset menu state
-        $('#side-menu').removeAttr('style');
-    }
+	if (!$('body').hasClass('mini-navbar') || $('body').hasClass('body-small')) {
+		// Hide menu in order to smoothly turn on when maximize menu
+		$('#side-menu').hide();
+		// For smoothly turn on menu
+		setTimeout(
+			function () {
+				$('#side-menu').fadeIn(400);
+			}, 200);
+	} else if ($('body').hasClass('fixed-sidebar')) {
+		$('#side-menu').hide();
+		setTimeout(
+			function () {
+				$('#side-menu').fadeIn(400);
+			}, 100);
+	} else {
+		// Remove all inline style from jquery fadeIn function to reset menu state
+		$('#side-menu').removeAttr('style');
+	}
 }
 
 // Dragable panels
 function WinMove() {
-    var element = "[class*=col]";
-    var handle = ".ibox-title";
-    var connect = "[class*=col]";
-    $(element).sortable(
-        {
-            handle: handle,
-            connectWith: connect,
-            tolerance: 'pointer',
-            forcePlaceholderSize: true,
-            opacity: 0.8
-        })
-        .disableSelection();
+	var element = "[class*=col]";
+	var handle = ".ibox-title";
+	var connect = "[class*=col]";
+	$(element).sortable(
+		{
+			handle: handle,
+			connectWith: connect,
+			tolerance: 'pointer',
+			forcePlaceholderSize: true,
+			opacity: 0.8
+		})
+	.disableSelection();
 }
