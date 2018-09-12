a230ae4ad8b6e867ccb828df45fc38122a08b588
gary80212@gmail.com <gary80212@gmail.com>
2018-09-13 01:21:46 +0800

    feature<frontend>finish design index

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,41 +2,41 @@ class DashboardsController < ApplicationController
   before_action :authenticate_user!
   require 'json'
 
-  def ga
-    require 'google/apis/analyticsreporting_v4'
+  # def ga
+  #   require 'google/apis/analyticsreporting_v4'
 
-    analytics = Google::Apis::AnalyticsreportingV4::AnalyticsReportingService.new
-    analytics.authorization = current_user.google_token
+  #   analytics = Google::Apis::AnalyticsreportingV4::AnalyticsReportingService.new
+  #   analytics.authorization = current_user.google_token
 
-    date_range = Google::Apis::AnalyticsreportingV4::DateRange.new(start_date: '7DaysAgo', end_date: 'today')
-    metric = Google::Apis::AnalyticsreportingV4::Metric.new(expression: 'ga:sessions', alias: 'sessions')
-    dimension = Google::Apis::AnalyticsreportingV4::Dimension.new(name: 'ga:browser')
+  #   date_range = Google::Apis::AnalyticsreportingV4::DateRange.new(start_date: '7DaysAgo', end_date: 'today')
+  #   metric = Google::Apis::AnalyticsreportingV4::Metric.new(expression: 'ga:sessions', alias: 'sessions')
+  #   dimension = Google::Apis::AnalyticsreportingV4::Dimension.new(name: 'ga:browser')
     
-    request = Google::Apis::AnalyticsreportingV4::GetReportsRequest.new(
-      report_requests: [Google::Apis::AnalyticsreportingV4::ReportRequest.new(
-      view_id: '55621750',
-      metrics: [metric],
-      dimensions: [dimension],
-      date_ranges: [date_range]
-      )]
-    ) 
-    @response = analytics.batch_get_reports(request)
-    @response.reports
-    render json: {
-      data: @response
-    }
+  #   request = Google::Apis::AnalyticsreportingV4::GetReportsRequest.new(
+  #     report_requests: [Google::Apis::AnalyticsreportingV4::ReportRequest.new(
+  #     view_id: '55621750',
+  #     metrics: [metric],
+  #     dimensions: [dimension],
+  #     date_ranges: [date_range]
+  #     )]
+  #   ) 
+  #   @response = analytics.batch_get_reports(request)
+  #   @response.reports
+  #   render json: {
+  #     data: @response
+  #   }
     
-  end
+  # end
 
   def mailchimp
     @campaigns = Mailchimp.campaigns('2018-08-01', '2018-09-01')
   end
 
-  def fb
-    require 'koala'
-    @graph = Koala::Facebook::API.new(CONFIG.FB_TOKEN)
-    @fans = @graph.get_object("278666028863859/insights/page_fans")
-  end
+  # def fb
+  #   require 'koala'
+  #   @graph = Koala::Facebook::API.new(CONFIG.FB_TOKEN)
+  #   @fans = @graph.get_object("278666028863859/insights/page_fans")
+  # end
   
   def alexa
     @sein = Alexa.data('seinsights.asia')
