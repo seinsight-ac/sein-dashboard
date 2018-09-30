8ce2fba99ade19fbcc9e692c5631816ef107c7f2
tsang <B10423013@gemail.yuntech.edu.tw>
2018-09-30 17:30:20 +0800

    fix job

--- app/jobs/grab_mailchimp_job.rb
+++ app/jobs/grab_mailchimp_job.rb
@@ -4,28 +4,6 @@ class GrabMailchimpJob < ApplicationJob
   def perform(*args)
     count = MailchimpDb.count
 
-    class << self
-      def set_mailchimp_db(campaigns)
-        campaigns.each do |c|
-          if c["send_time"].split('T').first != MailchimpDb.last.date.to_s.split(" ").first
-
-            link = Mailchimp.click_details(c["id"])
-            MailchimpDb.create(
-              date: c["send_time"], 
-              title: c["settings"]["subject_line"],
-              email_sent: c["emails_sent"],
-              open: c["report_summary"]["opens"],
-              open_rate: c["report_summary"]["open_rate"],
-              click: c["report_summary"]["subscriber_clicks"],
-              click_rate: c["report_summary"]["click_rate"],
-              most_click_title: link["url"],
-              most_click_time: link["unique_clicks"]
-              )
-          end
-        end
-      end
-    end
-
     before = (Date.today - 2)
     since = (Date.today - 7)
 
@@ -34,5 +12,25 @@ class GrabMailchimpJob < ApplicationJob
     set_mailchimp_db(campaigns)
 
     puts "add #{MailchimpDb.count - count} mailchimp data"
+  end
+
+  def set_mailchimp_db(campaigns)
+    campaigns.each do |c|
+      if c["send_time"].split('T').first != MailchimpDb.last.date.to_s.split(" ").first
+
+        link = Mailchimp.click_details(c["id"])
+        MailchimpDb.create(
+          date: c["send_time"], 
+          title: c["settings"]["subject_line"],
+          email_sent: c["emails_sent"],
+          open: c["report_summary"]["opens"],
+          open_rate: c["report_summary"]["open_rate"],
+          click: c["report_summary"]["subscriber_clicks"],
+          click_rate: c["report_summary"]["click_rate"],
+          most_click_title: link["url"],
+          most_click_time: link["unique_clicks"]
+          )
+      end
+    end
   end
 end
